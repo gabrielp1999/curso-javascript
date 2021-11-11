48543bab2e77a8381fc262c53e8d807f53574f51
Gabriel Pereira <gabriel.gsm2017@gmail.com>
2021-11-11 18:17:42 -0300

    Concatenando arrays

---
+++ modulo-5/aula-83.js
@@ -0,0 +1,8 @@
+
+const num1 = [1, 2, 3];
+const num2 = [4, 5, 6];
+
+//      ... rest  => ...spred
+const num3 = [...num1, ...num2];
+console.log(num3);
+// const total = nums1.concat(nums2, [7, 8, 9], 'Gabriel'); // para concaternar os valores do array
