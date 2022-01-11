41f3bbbdd2e5dfc9521ef1870d5e91cad6921adc
Gabriel <gabriel.gsm2017@gmail.com>
2022-01-10 21:24:33 -0300

    aula-2

---
+++ modulo-10/node/aula-2/app.js
@@ -0,0 +1,18 @@
+const path = require('path')
+console.log(path.resolve(__filename))
+
+// serve para mostrar aparti da raiz onde o arquivo estar monstrando o arquivo tambem
+// console.log(__filename);
+
+// serve para mostrar aparti da raiz a pasta onde o arquivos estar
+// console.log(__dirname)
+
+// const Cachorro = require("./mod");
+
+// const cachorrinho = new Cachorro("Amarelão")
+// cachorrinho.latir()
+
+// require serve para importar o arquivo ou variavel
+// const multiplicacao = require('./mod');
+
+// console.log(multiplicacao(10, 2))
---
+++ modulo-10/node/aula-2/mod.js
@@ -0,0 +1,15 @@
+module.exports = class Cachoro {
+    constructor(nome) {
+        this.nome = nome;
+
+    }
+    latir() {
+        console.log(`${this.nome} esta fazendo au au`)   
+    }
+}
+
+
+// module.exports = function(x, y) {
+//     return x * y;
+// }
+
