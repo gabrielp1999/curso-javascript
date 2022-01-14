3b8eab495ff38035877f2ecc5bfe101f35d9c74b
Gabriel <gabriel.gsm2017@gmail.com>
2022-01-14 17:03:05 -0300

    Componentes

--- modulo-14/lista-de-tarefas/src/App.js
+++ modulo-14/lista-de-tarefas/src/App.js
@@ -1,7 +1,8 @@
-import React from "react";
-import './App.css'
+import React from 'react';
+import Main from './components/Main';
+
+import './App.css';
 
 export default function App() {
-  return <h1>Hello world!</h1>
+  return <Main />;
 }
-
