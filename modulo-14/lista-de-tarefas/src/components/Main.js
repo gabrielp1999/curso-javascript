8f3cafe3ea68bfda78be385c75e3376a3b4df130
Gabriel <gabriel.gsm2017@gmail.com>
2022-01-18 17:29:08 -0300

    Separando os componentes

--- modulo-14/lista-de-tarefas/src/components/Main.js
+++ modulo-14/lista-de-tarefas/src/components/Main.js
@@ -1,12 +1,9 @@
 import React, { Component } from "react";
 
+import Form from './Form';
+import Tarefas from './Tarefas';
+
 import './Main.css';
-
-// form
-import { FaPlus } from 'react-icons/fa';
-
-// tarefas
-import { FaEdit, FaWindowClose } from 'react-icons/fa';
 
 export default class Main extends Component {
 
@@ -90,21 +87,18 @@ export default class Main extends Component {
       <div className="main">
         <h1>Lista de tarefas</h1>
 
-        <form onSubmit={this.handleSubmit} action="" className="form">
-          <input onChange={this.handleChange} type="text" value={novaTarefa} />
-          <button type="submit"><FaPlus /></button>
-        </form>
+      <Form
+        handleSubmit={this.handleSubmit}
+        handleChange={this.handleChange}
+        novaTarefa={novaTarefa}
+      />
 
-        <ul className="tarefas">
-          {tarefas.map((tarefa, index) => (
-            <li key={tarefa}>{tarefa}
-              <span>
-                <FaEdit onClick={(e)=> this.handleEdit(e, index)} className="edit"/>
-                <FaWindowClose onClick={(e)=> this.handleDelete(e, index)} className="delete"/>
-              </span>
-            </li>
-          ))}
-        </ul>
+      <Tarefas
+        tarefas={tarefas}
+        handleEdit={this.handleEdit}
+        handleDelete={this.handleDelete}
+      />
+
       </div>
     );
   }
