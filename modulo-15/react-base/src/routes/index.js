74ef00297e551efa3b377bd47c3f6d151a02f01a
Gabriel <gabriel.gsm2017@gmail.com>
2022-01-25 16:44:31 -0300

    My Route

--- modulo-15/react-base/src/routes/index.js
+++ modulo-15/react-base/src/routes/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
-import { Route, Routes  } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
+import MyRoute from './MyRoute';
 import Login from '../pages/Login';
 import Page404 from '../pages/page404';
 
 
 export default function Router() {
   return(
-      <Routes>
-        <Route exact path="/" element={<Login />} />
-        <Route path="*" element={<Page404 />} />
-      </Routes>
+      <Switch>
+        <MyRoute exact path="/" component={Login} isClosed />
+        <MyRoute path="*" component={Page404} />
+      </Switch>
   );
 }
+
